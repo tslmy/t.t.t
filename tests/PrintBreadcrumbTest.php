0d8def4a6f82854475294e4343184d95d93bb722
Mingyang Li <tslmy@outlook.com>
2021-01-26 16:41:13 -0800

    added unit test for print_breadcrumb

---
+++ tests/PrintBreadcrumbTest.php
@@ -0,0 +1,41 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require "src/commons.php";
+
+final class PrintBreadcrumbTest extends TestCase
+{
+    /**
+     * @dataProvider breadcrumbTestDataProvider
+     */
+    public function testOutputs($crumbs, $cur_path, $want): void
+    {
+        $got = print_breadcrumb($crumbs, $cur_path);
+        $this->assertEquals($want, $got);
+    }
+    public function breadcrumbTestDataProvider(): array
+    {
+        return [
+            [
+                ['a', 'b', 'c'],
+                '/a/b',
+                '<a href="index.php">Home</a> &gt; <a href="index.php?folder=%2Fa">a</a> &gt; b &gt; <a href="index.php?folder=%2Fa%2Fb%2Fc">c</a>',
+            ],
+            [
+                ['a', 'b', 'c'],
+                '',
+                '<a href="index.php">Home</a> &gt; <a href="index.php?folder=%2Fa">a</a> &gt; <a href="index.php?folder=%2Fa%2Fb">b</a> &gt; <a href="index.php?folder=%2Fa%2Fb%2Fc">c</a>',
+            ],
+            [
+                [],
+                '',
+                '<a href="index.php">Home</a>',
+            ],
+            [
+                [],
+                '/a/b',
+                '<a href="index.php">Home</a>',
+            ],
+        ];
+    }
+}
+?>
