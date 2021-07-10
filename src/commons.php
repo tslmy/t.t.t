b8a2e99d67ad82b0afaf7919d93c9c18f50aa3fd
Mingyang Li <tslmy@outlook.com>
2021-07-10 10:27:49 -0700

    cs fixers

--- src/commons.php
+++ src/commons.php
@@ -1,4 +1,5 @@
 <?php
+
 define('SITE_NAME', "tslmy's blog");				//This defines the site's name.
 define('SITE_DESC', "A dummy blog for demonstrating the database-free blog engine, t.t.t.");				//This defines the site's description.
 define('ITEMS_DISPLAYED_PER_PAGE', 2);		//How many posts are to be shown on each page of the index.
--- tests/PrintBreadcrumbTest.php
+++ tests/PrintBreadcrumbTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 require "src/commons.php";
